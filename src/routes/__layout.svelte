a1616f045542ab48f53ec1f37dab7cb30bec40f4
dinisrosas <dinisrosas@hotmail.com>
2022-04-16 00:00:42 +0100

    add: firebase analytics

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,8 @@
 <script>
 	import Footer from '$lib/footer.svelte';
-	import { GoogleAnalytics } from '@beyonk/svelte-google-analytics';
 	import SEO from '$lib/seo.svelte';
 	import '../styles/global.css';
 </script>
-
-<GoogleAnalytics properties={['G-06KK95QYD3']} />
 
 <SEO />
 
