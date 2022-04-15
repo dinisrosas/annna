68355a3ba4b13cbb6f4d3c83bbd410e9cb0763bb
dinisrosas <dinisrosas@hotmail.com>
2022-04-15 17:21:38 +0100

    add: typeform form

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import Button from '$lib/button.svelte';
+
 	import gsap from 'gsap';
 	import ScrollTrigger from 'gsap/dist/ScrollTrigger.js';
 	import { onMount } from 'svelte';
@@ -15,7 +17,7 @@
 			y: 15,
 			opacity: 0,
 			ease: 'power2.out',
-			stagger: 0.06,
+			stagger: 0.06
 		});
 
 		gsap.from('#mockup-1', {
@@ -55,23 +57,9 @@
 
 		<hr class="stagger" />
 
-		<a href="mailto:dinisrosas@gmail.com" target="_blank" class="stagger">
-			<button
-				><svg
-					xmlns="http://www.w3.org/2000/svg"
-					aria-hidden="true"
-					role="img"
-					width="1.25rem"
-					height="1.25rem"
-					preserveAspectRatio="xMidYMid meet"
-					viewBox="0 0 32 32"
-					><path
-						fill="currentColor"
-						d="M28 6H4a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h24a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2Zm-2.2 2L16 14.78L6.2 8ZM4 24V8.91l11.43 7.91a1 1 0 0 0 1.14 0L28 8.91V24Z"
-					/></svg
-				>Send us your proposal</button
-			>
-		</a>
+		<div class="button stagger">
+			<Button />
+		</div>
 	</div>
 
 	<div class="mockups">
@@ -109,7 +97,7 @@
 		margin: 0.5rem 0;
 	}
 
-	a {
+	.button {
 		float: left;
 		margin-top: 1rem;
 	}
